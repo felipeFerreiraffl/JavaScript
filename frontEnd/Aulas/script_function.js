54a1b206d55c5752e71f9959827e66de655636f8
unknown <felipado2x0@gmail.com>
2024-03-27 22:24:40 -0300

    learned about api functions and consumption

--- frontEnd/Aulas/script_function.js
+++
@@ -1,8 +0,0 @@
-// Esse arquivo vai pegar todos os códigos do JavaScript
-var contador = 0;
-function converterTemp() {
-    // Forma de busca de uma parte do HTML por meio do ID
-    // innerText = inserir um texto
-    contador++;
-    document.getElementById("saída").innerText = contador;
-}
